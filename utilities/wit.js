4382d3a0d6efed6b239350e0a4c36935a44cf7ca
Deniz <candas@in.tum.de>
2018-05-03 18:54:44 +0200

    gitplz

--- utilities/wit.js
+++ utilities/wit.js
@@ -1,7 +1,7 @@
 const { Wit, log } = require("node-wit")
 
 const WitClient = new Wit({
-  accessToken: "XEIUUVCOS5OKC2MRNCKKMEQI6XTB3FFR",
+  accessToken: "DHFJEMDGD3X56BWAXXPKODG3V27HUXAP",
   logger: new log.Logger(log.DEBUG)
 })
 
